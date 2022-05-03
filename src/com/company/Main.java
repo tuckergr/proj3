5aa18ac499c45784df8798f467cb052ef9b0cfe2
Tucker H Gragg 11th WHS <078891@SWCSD.US>
2022-05-03 13:17:58 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+class Main {
+    public static void main(String[] args) {
+        TaskInput collection = new TaskInput();
+        ViewTask view = new ViewTask(collection);
+        view.run();
+    }
+}
